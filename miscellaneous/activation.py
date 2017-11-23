656ebae01b001acf17473afb97a4784d1ec0d1a5
Arun Kumar Krishnan <emailarunkumar@gmail.com>
2017-11-23 23:44:07 +0530

    Added sigmoid and relu activation impl

---
+++ miscellaneous/activation.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+import matplotlib.pylab as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+y_sigmoid = sigmoid(x)
+y_relu = relu(x)
+
+plt.plot(x, y_sigmoid, label='Sigmoid', color='k', lw=1, ls='--')
+plt.plot(x, y_relu, label='ReLU', color='k', lw=1, linestyle='-.')
+plt.ylim(-0.1, 1.1)
+plt.legend()
+plt.show()
